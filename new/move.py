1d37049a684a7ebc4a0df488c912184e44a5cb0d
NeuronAI-Git <134161129+NeuronAI-Git@users.noreply.github.com>
2024-11-21 14:15:49 -0500

    Create move.py

---
+++ new/move.py
@@ -0,0 +1,12 @@
+import time
+from picarx import Picarx
+
+px = Picarx()
+px.set_dir_servo_angle(20)
+time.sleep(0.5)
+px.set_dir_servo_angle(-20)
+time.sleep(0.5)
+px.set_dir_servo_angle(0)
+time.sleep(0.5)
+px.forward(10)
+px.backward(10)
