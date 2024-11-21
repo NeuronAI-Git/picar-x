91d86bbebc5c4e14b53495fabcb813b5a3aeabfc
NeuronAI-Git <134161129+NeuronAI-Git@users.noreply.github.com>
2024-11-21 03:53:33 -0500

    Update test1.py

--- new/test1.py
+++ new/test1.py
@@ -1,1 +1,19 @@
-print("test1 and new added correctly + 1")
+import pollinations
+from robot_hat import TTS
+
+model = pollinations.text(
+  model=pollinations.text_default,
+  contextual=True
+)
+tts = TTS()
+
+def main():
+    tts.lang("en-US")
+    tts.say("wow thats cool bro")
+    while True:
+        response = model.generate(input(">>> ")).text
+        print(response)
+        tts.say(response)
+
+if __name__ == "__main__":
+    main()
