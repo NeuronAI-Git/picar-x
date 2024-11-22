55241e2c68fcd1e0c11c8179b07ac05034ad11dc
NeuronAI-Git <134161129+NeuronAI-Git@users.noreply.github.com>
2024-11-21 22:03:01 -0500

    Update ts.py

--- new/ts.py
+++ new/ts.py
@@ -1,11 +1,5 @@
 import pimodule
 
-pimodule.SPEAK.pico2wave("Hi there! I am looking around.")
-
-for _ in range(2):
-    pimodule.CAR.set_pan(30, 1)
-    pimodule.CAR.set_pan(-30, 1)
-
-pimodule.CAR.set_pan(0, 1)
-pimodule.SPEAK.pico2wave("Nice room!", 0.5)
-pimodule.LLM.generate("So, how are you?", 1)
+pimodule.CAR.set_motor_1(10)
+pimodule.CAR.set_motor_2(-10)
+pimodule.CAR.stop(2)
