b2f85942e64f25259271a372bed9d3e683cd7016
NeuronAI-Git <134161129+NeuronAI-Git@users.noreply.github.com>
2024-11-21 21:34:55 -0500

    Update ts.py

--- new/ts.py
+++ new/ts.py
@@ -2,3 +2,11 @@ import pimodule
 
 pimodule.CAR.forward(5)
 pimodule.CAR.stop(2)
+
+pimodule.CAR.backward(5)
+pimodule.CAR.stop(2)
+
+pimodule.CAR.set_dir(-30)
+pimodule.CAR.forward(5)
+pimodule.CAR.stop(4)
+pimodule.CAR.set_dir(0)
