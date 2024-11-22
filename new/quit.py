632daa8ae2dab75474c08403b0ba025fdb1440ad
NeuronAI-Git <134161129+NeuronAI-Git@users.noreply.github.com>
2024-11-21 23:18:12 -0500

    Create quit.py

---
+++ new/quit.py
@@ -0,0 +1,10 @@
+import pimodule
+
+pimodule.CAR.set_motor_1(0)
+pimodule.CAR.set_motor_2(0)
+pimodule.CAR.set_pan(0)
+pimodule.CAR.set_tilt(0)
+pimodule.CAR.set_dir(0)
+pimodule.CAR.stop()
+pimodule.MUSIC.disable()
+pimodule.VISION.camera_close()
