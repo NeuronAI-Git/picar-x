0bfad61cde09a0f0b86e4ede1734e00f004b7f2b
NeuronAI-Git <134161129+NeuronAI-Git@users.noreply.github.com>
2024-12-05 05:32:30 -0500

    Update default.py

--- new/default.py
+++ new/default.py
@@ -34,10 +34,6 @@ def main(
 ) -> None:
     print(">>> main_start")
     
-    
-    
-    print(">>> main_end")
-
 #  ------------------------------------------
 
 #  @final  ----------------------------------
@@ -46,31 +42,49 @@ def final(
 ) -> None:
     print(">>> final_start")
     
+    pi.CAR.set_dir(0)
+    pi.CAR.set_tilt(0)
+    pi.CAR.set_pan(0)
+    pi.CAR.set_cliff([0, 0, 0])
+    pi.CAR.set_speed(0)
+    pi.CAR.set_motor_1(0)
+    pi.CAR.set_motor_2(0)
+    pi.CAR.set_grayscale([0, 0, 0])
+    pi.CAR.set_line([0, 0, 0])
+    pi.CAR.set_power(0)
+    pi.CAR.forward(0)
     
     print(">>> final_end")
 #  ------------------------------------------
 
 #  @run  ------------------------------------
 if __name__ == "__main__":
-    time.sleep(0)
-    print(
-        f"[{datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')}]",
-        "PI_START:", pi.__name__,
-        f">>> {__name__}"
-    )
-    (
-        _ln(),
-        setup(),
-        _ln(),
-        main(),
-        _ln(),
-        final(),
+    try:
+        time.sleep(0)
+        print(
+            f"[{datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')}]",
+            "PI_START:", pi.__name__,
+            f">>> {__name__}"
+        )
+        (
+            _ln(), setup(),
+            _ln(), main(),
+        )
+
+    except Exception as e:
+        print(e)
+        
+    finally:
+        (
+            print(">>> main_end"),
+            _ln(), final(),
+            _ln()
+        )
+        print(
+            f"[{datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')}]",
+            "PI_END:", pi.__name__,
+            f">>> {__name__}"
+        )
+        time.sleep(0)
         _ln()
-    )
-    print(
-        f"[{datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')}]",
-        "PI_END:", pi.__name__,
-        f">>> {__name__}"
-    )
-    time.sleep(0)
 #  ------------------------------------------
