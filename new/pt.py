9ab5765e7b7bf702b3a96f87cafc601851e23dfc
NeuronAI-Git <134161129+NeuronAI-Git@users.noreply.github.com>
2024-12-05 04:56:28 -0500

    Update pt.py

--- new/pt.py
+++ new/pt.py
@@ -1,25 +1,64 @@
-import pimodule as pi
-import datetime
+#  @imports  --------------------------------
 import time
+import datetime
+import pimodule as pi
+#  ------------------------------------------
 
-pi.init(music=False, vision=False)
+#  @setup  ----------------------------------
+def setup(
+    *args, **kwargs
+) -> None:
+    print(">>> setup_start")
+    
+    # pi.init(vision=False, music=False, tts=False)
+    pi.init()
+    
+    print(">>> setup_end")
+#  ------------------------------------------
 
-def main():
-    time.sleep(0) # Start
-    
-    pi.CAR.set_tilt(30)
-    
-    for _ in range(10):
-        print(pi._picar.ultrasonic.read())
-        time.sleep(0.2)
-        
-    time.sleep(1)
-    pi.CAR.set_tilt(30)
-    
-    time.sleep(0)  # End
+#  @main  -----------------------------------
+def main(
+    *args, **kwargs
+) -> None:
+    print(">>> main_start")
     
     
     
+    print(">>> main_end")
+
+#  ------------------------------------------
+
+#  @final  ----------------------------------
+def final(
+    *args, **kwargs
+) -> None:
+    print(">>> final_start")
+    
+    
+    print(">>> final_end")
+#  ------------------------------------------
+
+#  @run  ------------------------------------
 if __name__ == "__main__":
-    print(f"[{datetime.datetime().strftime('%Y-%m-%d %I:%M %p')}] start: ", pi, f">>> {__name__}")
-    main()
+    time.sleep(0)
+    print(
+        f"[{datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')}]",
+        "PI_START:", pi.__name__,
+        f">>> {__name__}"
+    )
+    (
+        print('------------------------------------------'),
+        setup(),
+        print('------------------------------------------'),
+        main(),
+        print('------------------------------------------'),
+        final(),
+        print('------------------------------------------')
+    )
+    print(
+        f"[{datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')}]",
+        "PI_END:", pi.__name__,
+        f">>> {__name__}"
+    )
+    time.sleep(0)
+#  ------------------------------------------
